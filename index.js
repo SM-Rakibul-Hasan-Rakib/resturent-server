0517d4e809990bef51db5bad81e2b002f936f19b
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-05-31 17:31:43 +0600

    cart post method done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -42,11 +42,26 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/carts", async (req, res) => {
-      const result = await cartCollection.find().toArray();
+    // table delete funcation
+    app.delete("carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
     // review collection
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
